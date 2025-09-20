852332c451e7931404179f6bbf2e48869b239cd1
bhavanachoudhari24 <bhavana.choudhari24@gmail.com>
2025-09-20 18:29:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-sample-batch-aws-devops-azure-29-20sep2025"
-    key = "main"
+    key = "lockID"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
